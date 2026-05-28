1e3b0c493f4736c017003f4c7b608d85544d5059
Fxxrz <78870557+Fxxrz@users.noreply.github.com>
2026-05-28 22:51:34 +0200

    Fix Asmond formula dependency order

--- Formula/asmond.rb
+++ Formula/asmond.rb
@@ -5,8 +5,8 @@ class Asmond < Formula
   sha256 "3547ef1d3183b9db6550c38d5ec4363490a4a3ae4b5f9282b005d6647948e1c9"
   license "MIT"
 
-  depends_on :macos
   depends_on arch: :arm64
+  depends_on :macos
   depends_on "python@3.14"
 
   def install
